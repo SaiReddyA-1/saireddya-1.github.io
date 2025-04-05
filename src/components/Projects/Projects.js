135da2dab5115f4a98ca4762c972b9b2be943809
SaiReddy <salvadorxy54@gmail.com>
2025-04-05 15:29:54 +0530

    update portfolio with new logos, personal information, and social media links

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -49,8 +49,8 @@ function Projects() {
               isBlog={false}
               title="Editor.io"
               description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              ghLink="https://github.com/SaiReddyA-1/Editor.io/"
+              demoLink="https://editor0saireddy.ccbp.tech/"              
             />
           </Col>
 
@@ -70,9 +70,9 @@ function Projects() {
               imgPath={suicide}
               isBlog={false}
               title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
+              description="Suicide is a major public health concern, and early detection of suicidal ideation can save lives. Our project leverages Natural Language Processing (NLP) to identify suicide-related posts and detect users exhibiting suicidal tendencies in cyberspace. By analyzing text patterns, sentiment, and linguistic cues, our AI-driven system can flag distress signals in social media posts, forums, and online communities."
               ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
+              demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" //<--------Please include a demo link here
             />
           </Col>
 
@@ -84,7 +84,7 @@ function Projects() {
               description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
               Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
               ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+              demoLink="https://blogs.soumya-jit.tech/"
             />
           </Col>
         </Row>
